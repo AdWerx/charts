7c8e0dae2404bf73a16cef9b19a58b08e3162899
Guillaume audic <audic.gui@gmail.com>
2020-07-02 12:28:06 +0200

    Fix separator key values for database host

--- awx/files/conf.d/environment.sh
+++ awx/files/conf.d/environment.sh
@@ -1,9 +1,9 @@
 DATABASE_USER={{ .Values.postgresql.postgresqlUsername }}
 DATABASE_NAME={{ .Values.postgresql.postgresqlDatabase }}
 {{- if .Values.postgresql.postgresqlHost }}
-DATABASE_HOST: {{ .Values.postgresql.postgresqlHost }}
+DATABASE_HOST={{ .Values.postgresql.postgresqlHost }}
 {{ else }}
-DATABASE_HOST: {{ .Release.Name }}-postgresql
+DATABASE_HOST={{ .Release.Name }}-postgresql
 {{ end -}}
 DATABASE_PORT={{ .Values.postgresql.service.port }}
 DATABASE_PASSWORD={{ .Values.postgresql.postgresqlPassword }}
